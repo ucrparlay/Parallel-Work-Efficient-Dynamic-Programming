690af3d1509face6d13365f69b93f77440792960
xindubawukong <xindubawukong@gmail.com>
2023-02-10 22:27:03 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 
+#include "parlay/internal/get_time.h"
+#include "parlay/primitives.h"
 #include "parlay/sequence.h"
 
 using namespace std;
 
 int main() {
-  parlay::sequence<int> a(100);
-  cout << a[0] << endl;
+  int n = 100000000;
+  parlay::sequence<unsigned int> a(n);
+  for (int i = 0; i < n; i++) {
+    a[i] = rand() % 200000000;
+  }
+  auto b = a;
+  parlay::internal::timer tt;
+  parlay::stable_sort_inplace(a);
+  cout << tt.next_time() << endl;
+  parlay::stable_integer_sort_inplace(parlay::make_slice(b),
+                                      [&](unsigned int x) { return x; });
+  cout << tt.stop() << endl;
+  for (int i = 0; i < n; i++) {
+    assert(a[i] == b[i]);
+  }
+  cout << "Pass!" << endl;
   return 0;
 }

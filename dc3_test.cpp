11cd1a120bf0c80caf88fce70ae4eddc7ca74f80
xindubawukong <xindubawukong@gmail.com>
2023-03-15 01:59:49 +0800

    add

--- dc3_test.cpp
+++ dc3_test.cpp
@@ -1,14 +1,19 @@
+#include "dc3.h"
+
 #include <iostream>
 #include <random>
 
-#include "dc3.h"
+#include "dc3_config.h"
+#include "gflags/gflags.h"
+#include "glog/logging.h"
 #include "parlay/internal/get_time.h"
 #include "parlay/primitives.h"
 #include "parlay/sequence.h"
 
 using namespace std;
 
-bool good(int n, int alpha) {
+bool TestDC3(int n, int alpha) {
+  LOG(INFO) << "n: " << n << endl;
   std::mt19937 rng(0);
   auto a = parlay::sequence<unsigned int>(n);
   for (int i = 0; i < n; i++) {
@@ -18,7 +23,8 @@ bool good(int n, int alpha) {
   for (int rr = 0; rr < 5; rr++) {
     parlay::internal::timer tt;
     auto sa1 = suffix_array(a);
-    x += tt.next_time();
+    x += tt.stop();
+    tt.start();
     auto sa2 = DC3(a);
     y += tt.stop();
     if (rr > 0) continue;
@@ -27,12 +33,20 @@ bool good(int n, int alpha) {
     }
   }
   x /= 5, y /= 5;
-  cout << x << endl << y << endl << "rate: " << y / x << endl;
+  LOG(INFO) << "parlay SA:" << x << endl;
+  LOG(INFO) << "DC3: " << y << endl;
+  LOG(INFO) << "rate: " << y / x << endl;
   return true;
 }
 
-int main() {
-  assert(good(100000, 128));
-  cout << "SA test pass!" << endl;
+DEFINE_int32(n, 10000, "n");
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  FLAGS_log_dir = CMAKE_CURRENT_SOURCE_DIR"/logs";
+  FLAGS_alsologtostderr = 1;
+  google::InitGoogleLogging(argv[0]);
+  CHECK(TestDC3(FLAGS_n, 128));
+  LOG(INFO) << "Test Pass!" << endl;
   return 0;
 }
